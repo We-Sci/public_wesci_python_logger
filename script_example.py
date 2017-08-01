d6f8399e6632b760fb640d96a4027f3a279aa722
Yaron Hagai <yaron@we-sci.com>
2017-08-01 11:42:30 +0300

    removed figures from 0.1.0

--- script_example.py
+++ script_example.py
@@ -1,5 +1,4 @@
 import wesci
-from matplotlib.pyplot import plot
 
 logger = wesci.Logger(
     user_id='Crazy Scientist',
@@ -13,15 +12,11 @@ a = 3
 logger.add_input_params({'a': a})
 logger.add_input_files({'input_csv': 'input.csv'})
 
-
 # do some math
 b = a**2
 
 logger.add_output_files({'output_csv': 'output.csv'})
 logger.add_output_params({'b': b})
-
-plot([1, 2, 3], [4, 5, 6])
-logger.add_output_figure('out_fig')
 
 # log
 logger.log()
